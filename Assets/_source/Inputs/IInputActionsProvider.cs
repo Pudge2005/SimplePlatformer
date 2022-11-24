0c05a88bfb0057bd5c5f911f47cba2afc73e5632
hooyamba <goseabas@gmail.com>
2022-11-24 08:25:01 +0300

    Revert "shitty realization and temporary inputs handling"
    This reverts commit a3756a44b6ae414c82079d40cf237c894e18e20c.

--- Assets/_source/Inputs/IInputActionsProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine.InputSystem;
-
-namespace Game
-{
-    public interface IInputActionsProvider<TControls> where TControls : IInputActionCollection
-    {
-        TControls InputActions { get; }
-    }
-}
