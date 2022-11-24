a3756a44b6ae414c82079d40cf237c894e18e20c
hooyamba <goseabas@gmail.com>
2022-11-24 08:14:47 +0300

    shitty realization and temporary inputs handling

---
+++ Assets/_source/CharacterController/ICharacterController2D.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public interface ICollisionDetector2D
+    {
+        int Detect(Collider2D[] buffer);
+    }
+
+    public interface ICollisionResolver2D
+    {
+        Vector2 Resolve(ReadOnlyMemory<Collider2D> collisions);
+    }
+    public interface ICharacterController2DState
+    {
+        CollisionFlags CollisionFlags { get; }
+        ReadOnlyMemory<Collider2D> Collisions { get; }
+
+
+        event System.Action<ICharacterController2DState> OnStateUpdated;
+    }
+
+
+    public interface ICharacterController2D
+    {
+        ICharacterController2DState State { get; }
+
+
+        void Move(Vector2 move);
+    }
+}
---
+++ Assets/_source/Inputs/IInputActionsProvider.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.InputSystem;
+
+namespace Game
+{
+    public interface IInputActionsProvider<TControls> where TControls : IInputActionCollection
+    {
+        TControls InputActions { get; }
+    }
+}
